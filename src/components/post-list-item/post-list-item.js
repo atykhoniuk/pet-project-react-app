3eea615fd2b664b898a35f877d54678dc8716bee
Aleksandr Tykhoniuk <tihonyuk1999@gmail.com>
2021-09-23 18:26:27 +0300

    Learn state in React. Work with destructuring of object

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,23 +1,62 @@
-import React from "react";
+import React, {Component} from "react";
 
+export  default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+            like: false
+        };
 
-const PostListItem = () => {
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hello World
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }));
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }));
+    }
+
+    render() {
+        const {label} = this.props
+        const {important, like} = this.state;
+        let classNames = 'app-list-item d-flex justify-content-between';
+
+        if (important) {
+            classNames += ' important'
+        }
+
+        if (like) {
+            classNames += ' like'
+        }
+
+        return (
+            <div className={classNames}>
+            <span
+                className="app-list-item-label"
+                onClick={this.onLike}>
+                {label}
             </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button type="button" className="btn-star btn-sm">
-                    <i className="bi bi-star-fill"/>
-                </button>
-                <button type="button" className="btn-trash btn-sm">
-                    <i className="bi bi-trash-fill"/>
-                </button>
-                <i className="bi bi-heart-fill"/>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button
+                        type="button"
+                        className="btn-star btn-sm"
+                        onClick={this.onImportant}>
+                        <i className="bi bi-star-fill"/>
+                    </button>
+                    <button type="button" className="btn-trash btn-sm">
+                        <i className="bi bi-trash-fill"/>
+                    </button>
+                    <i className="bi bi-heart-fill"/>
+                </div>
             </div>
-        </li>
-    )
+        )
+    }
 }
-
-export default PostListItem;
